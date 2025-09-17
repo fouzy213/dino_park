cb199fc5267a791c841222823d40bae67ede14a1
fouzy213 <chikouchefouzy@gmail.com>
2025-09-17 22:58:46 +0200

    view_recap

--- controllers/FormController.ts
+++ controllers/FormController.ts
@@ -23,18 +23,22 @@ public async submitForm() {
       // Création de la réservation
       const id_user0 = 1;
       const reservationRepo = new ReservationRepository();
-    const reservations = await reservationRepo.ajouterReservation(dateInput, total,  id_user0);
+    const reservations = await reservationRepo.ajouterReservation(dateInput, total,id_user0  );
     console.log("Réservation ajoutée :", reservations);
 
     // Passer les données à la vue récapitulative
-    this.response.render("recap.ejs", {
-      reservations,
-      total,
-      date: dateInput,
-      adult,
-      child,
-      senior,
-    });
+  this.response.render("recap.ejs", {
+  reservations,
+  total,
+  date: dateInput,
+  adult,
+  child,
+  senior,
+  email: this.request.body.email,
+  phone: this.request.body.phone,
+});
+
+
   } catch (error) {
     console.error("Erreur lors de la soumission du formulaire :", error);
     this.response.status(500).send("Erreur serveur");
